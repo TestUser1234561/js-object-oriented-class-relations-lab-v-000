461af993e922690d2ba9694a5450201c08913b42
Morgan Fudge <mfudge92@gmail.com>
2018-03-18 14:13:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ class Trip {
     this.id = ++tripId;
     this.driverId = driver.id;
     this.passengerId = passenger.id;
+    this.driver = driver;
+    this.passenger = passenger;
     store.trips.push(this)
   }
 }
