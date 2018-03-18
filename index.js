c829b0294abe66536162214ef81391338a8c62c5
Morgan Fudge <mfudge92@gmail.com>
2018-03-18 14:10:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-let store = { drivers: [] }
-let id = 0;
+let store = { drivers: [], passengers: [] };
+let driverId = 0;
+let passengerId = 0;
 
 class Driver {
   constructor(name) {
     this.name = name;
-    this.id = ++id;
+    this.id = ++driverId;
     store.drivers.push(this)
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = ++passengerId;
+    store.passengers.push(this)
   }
 }
